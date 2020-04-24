34b39ded40eaf0642186dc725c7e2a69521704cb
legianha <doantrihung592@gmail.com>
2020-04-24 12:43:39 +0700

    second commit

--- App.js
+++ App.js
@@ -1,19 +1,113 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants'
+import { render } from 'react-dom';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      min: 25,
+      second: 0,
+      bigTime: true,
+    }
+  }
+
+  startDecrease(){
+    console.log("this is did count");
+    this.interval = setInterval(this.decrease, 1000);
+  }
+
+  decrease = () => {
+    if (this.state.second>0){
+      this.setState(prevState => ({
+        second: prevState.second-1,
+      }))
+    }
+    else{
+      this.setState(prevState => ({
+        min: prevState.min-1,
+        second: 59
+      }))
+    }
+    console.log(this.state.min, this.state.second)
+  }
+
+  stopDecrease(){
+    console.log("this is stop count");
+    clearInterval(this.interval);
+  }
+
+  resetTime(){
+    if (this.state.bigTime === true){
+      this.setState({
+        min: 25,
+        second: 0,
+      })
+    }
+    else{
+      this.setState({
+        min: 5,
+        second: 0,
+      });
+    }
+    this.stopDecrease();
+  }
+
+  switchTime(){
+    if (this.state.bigTime === true){
+      this.setState({
+        bigTime: false,
+        min: 5,
+        second: 0,
+      })
+    }
+    else{
+      this.setState({
+        bigTime: true,
+        min: 25,
+        second: 0,
+      }) 
+    }
+    this.stopDecrease()
+  }
+
+  render(){
+    return (
+      <View style={styles.allview}>
+          <Button
+            title="switch"
+            onPress={() => this.switchTime()}/>
+          <View style={{alignItems: 'center'}}>
+            <Text style={styles.text}>
+              {this.state.min}:{this.state.second}
+            </Text>
+          </View>
+          <Button 
+            title="start" 
+            onPress={() => this.startDecrease()}
+          />
+          <Button 
+            title="stop"
+            onPress={() => this.stopDecrease()}
+          />
+          <Button
+            title="reset"
+            onPress={() => this.resetTime()}
+            /> 
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  allview:{
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 48,
+  }
 });
